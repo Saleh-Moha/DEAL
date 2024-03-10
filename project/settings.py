d2683911c107954045803dd37e70f6903bd92d13
sale <sm3272692@gmail.com>
2024-03-10 17:05:28 +0200

    first commit

--- project/settings.py
+++ project/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-u2nsd-53&-nayw8akb#p*qg%ro&-krc7^au1hio2@b6rqjlcq5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
@@ -115,11 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/var/www/static/",
-]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
